44cfabcf3a3763d8277dda4aa3881bf26b0dcc44
Vishwesh Patil <vishveshpatil99@gmail.com>
2024-03-08 13:44:13 +0530

    added logo padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dev-Handy Games",
+  title: "Dev HG",
   description: "",
 };
 
